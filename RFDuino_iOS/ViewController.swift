0df36fca1f9f0582613550cd5bc3750d594558e5
Ryan A. Rodriguez <ryarodri@ucsc.edu>
2015-03-02 02:00:01 -0800

    Initial Commit

---
+++ RFDuino_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RFDuino_iOS
+//
+//  Created by Ryan A. Rodriguez on 3/2/15.
+//  Copyright (c) 2015 Ryan A. Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RFDuino_iOSTests/RFDuino_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  RFDuino_iOSTests.swift
+//  RFDuino_iOSTests
+//
+//  Created by Ryan A. Rodriguez on 3/2/15.
+//  Copyright (c) 2015 Ryan A. Rodriguez. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RFDuino_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
